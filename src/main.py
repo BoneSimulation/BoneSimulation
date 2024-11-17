2997b1dd5cb0da8ea459462021d756fdf2a30e4f
mathias <pfannenkuchen107@gmx.de>
2024-11-17 17:55:03 +0100

    commit to push my stats + added parallelization

--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 from mayavi import mlab
 import warnings
 import logging
+from multiprocessing import Pool
 
 from utils.utils import generate_timestamp, check_os
-
 
 timestamp = generate_timestamp()
 
@@ -20,36 +20,38 @@ formatter = logging.Formatter(f'{timestamp}: %(levelname)s : %(name)s : %(messag
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
-# Warnungen für falsche Farbprofile unterdrücken
 warnings.filterwarnings("ignore", message=".*iCCP: known incorrect sRGB profile.*")
+
+
+def load_image(filepath):
+    im = Image.open(filepath).convert("L")
+    return np.array(im)
+
+
+def process_image(image):
+    image_blurred = filters.gaussian(image, sigma=2, mode='constant', cval=0)
+    binary_image = image_blurred > filters.threshold_otsu(image_blurred)
+    return image_blurred, binary_image
 
 
 def process_and_visualize(directory):
     # Bilder laden und als 3D-Array speichern
-    data_list = []
-    for filename in sorted(os.listdir(directory)):
-        if filename.endswith(".tif"):
-            filepath = os.path.join(directory, filename)
-            im = Image.open(filepath).convert("L")  # "L" für Graustufen
-            im_array = np.array(im)
-            data_list.append(im_array)
-    data_array = np.array(data_list)
+    filepaths = [os.path.join(directory, filename) for filename in sorted(os.listdir(directory)) if
+                 filename.endswith(".tif")]
+    with Pool() as pool:
+        data_array = pool.map(load_image, filepaths)
+
+    data_array = np.array(data_array)
     print("Datenarray hat die Form:", data_array.shape)
-
-    # Gaußscher Filter anwenden
-    image_blurred_array = np.array([filters.gaussian(image, sigma=2, mode='constant', cval=0) for image in data_array])
-
-    # Binarisierung
-    binary_image_array = np.array([image > filters.threshold_otsu(image) for image in image_blurred_array])
-
-    # Bilder anzeigen
+    with Pool() as pool:
+        results = pool.map(process_image, data_array)
+    image_blurred_array, binary_image_array = zip(*results)
+    image_blurred_array = np.array(image_blurred_array)
+    binary_image_array = np.array(binary_image_array)
     plot_images(image_blurred_array, "Überarbeitete Bilder")
     plot_images(binary_image_array, "Binarisierte Bilder")
-
-    # Histogramm der Pixelwerte anzeigen
     plot_histogram(image_blurred_array)
 
-    # 3D-Visualisierung mit mayavi
     visualize_3d(image_blurred_array)
 
 
@@ -86,9 +88,6 @@ def plot_histogram(image_array):
 def visualize_3d(image_array):
     mlab.figure(size=(800, 800), bgcolor=(1, 1, 1))
     mlab.contour3d(image_array, contours=8, opacity=0.5, colormap='bone')
-    #mlab.volume_slice(image_array, plane_orientation='x_axes', slice_index=image_array.shape[0] // 2)
-    #mlab.volume_slice(image_array, plane_orientation='y_axes', slice_index=image_array.shape[1] // 2)
-    #mlab.volume_slice(image_array, plane_orientation='z_axes', slice_index=image_array.shape[2] // 2)
     mlab.savefig(f'/home/mathias/PycharmProjects/BoneSimulation/pictures/bone/mesh/plot_3d_{timestamp}.png')
 
 
@@ -101,10 +100,13 @@ if __name__ == "__main__":
     elif check_os() == "Linux":
         directory = "/home/mathias/PycharmProjects/BoneSimulation/data/dataset"
     elif check_os() == "MacOS":
-        directory = "/Users/mathias/PycharmProjects/BoneSimulation/data/dataset" # not familiar with macOS, please check
+        directory = "/Users/mathias/PycharmProjects/BoneSimulation/data/dataset"  # not familiar with macOS, please check
     else:
         print("Unknown OS!")
         directory = None
-    print(f"directory found in {directory}")
 
-    process_and_visualize(directory)
+    if directory is not None:
+        print(f"Directory found: {directory}")
+        process_and_visualize(directory)
+    else:
+        print("No valid directory found. Exiting.")
